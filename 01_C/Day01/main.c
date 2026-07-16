a4a55955f8e9df78abe0b2b8dcaec140d10a16b3
권준 <jun_mac@gwonjun-ui-MacBookPro.local>
2026-07-16 17:55:54 +0900

    Day 1: First C program

---
+++ 01_C/Day01/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("=== Vehicle Control System ===\n");
+    printf("Driver        : Kwon Jun\n");
+    printf("Engine Status : ON\n");
+    printf("Current Speed : 70 km/h\n");
+    printf("Target Speed  : 100 km/h\n");
+    printf("ACC Status    : READY\n");
+
+    return 0;
+}
